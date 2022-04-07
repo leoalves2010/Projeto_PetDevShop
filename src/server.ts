86d0bec1fd4cbc12c24c6de4ed61ec596c5c33bc
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-04-06 21:48:39 -0300

    Configuração do arquivo de rotas e implementação dos Controllers

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/mainRoutes';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(mainRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Página não encontrada! Erro 404.</h1>');
+})
 
 app.set('view engine', 'mustache');
 app.set('views', path.join(__dirname, 'views'));
